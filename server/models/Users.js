46d63b23d51f36dbcb4347c58aa8e2f1edc1e17c
root <root@DESKTOP-A93JRRE.localdomain>
2026-05-12 14:31:06 -0500

    cambios en proyecto 141

---
+++ server/models/Users.js
@@ -0,0 +1,56 @@
+const objectionUnique = require('objection-unique');
+const BaseModel = require('./BaseModel.cjs');
+const encrypt = require('../lib/secure.cjs');
+
+const unique = objectionUnique({ fields: ['email'] });
+
+module.exports = class User extends unique(BaseModel) {
+  static get tableName() {
+    return 'users';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['firstName', 'lastName', 'email', 'password'],
+      properties: {
+        id: { type: 'integer' },
+        firstName: { type: 'string', minLength: 1 },
+        lastName: { type: 'string', minLength: 1 },
+        email: { type: 'string', minLength: 1 },
+        password: { type: 'string', minLength: 3 },
+      },
+    };
+  }
+
+  set password(value) {
+    this.passwordDigest = encrypt(value);
+  }
+
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  verifyPassword(password) {
+    return encrypt(password) === this.passwordDigest;
+  }
+
+  static relationMappings = {
+    creatorTasks: {
+      relation: BaseModel.HasManyRelation,
+      modelClass: 'Task.cjs',
+      join: {
+        from: 'users.id',
+        to: 'tasks.creator_id',
+      },
+    },
+    executorTasks: {
+      relation: BaseModel.HasManyRelation,
+      modelClass: 'Task.cjs',
+      join: {
+        from: 'users.id',
+        to: 'tasks.executor_id',
+      },
+    },
+  };
+}
