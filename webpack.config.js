e172722337ae048418a52569da7506b93eca3af3
root <root@DESKTOP-A93JRRE.localdomain>
2026-05-12 14:37:15 -0500

    cambios en proyecto 141

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,11 @@
-// import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
-// const mode = process.env.NODE_ENV || 'development';
-
-// export default {
-//   mode,
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
-//       },
-//       {
-//         test: /\.(png|jpe?g|gif|svg)$/i,
-//         loader: 'file-loader',
-//         options: {
-//           name: '[name].[ext]',
-//         },
-//       },
-//     ],
-//   },
-//   plugins: [new MiniCssExtractPlugin()],
-// };
+// @ts-check
 
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-// ESTA LÍNEA ES LA QUE FALTA:
-import nodeExternals from 'webpack-node-externals'; 
 
 const mode = process.env.NODE_ENV || 'development';
 
 export default {
   mode,
-  target: 'node',
-  entry: './src/init.js',
-  // Aquí es donde se usa la variable importada arriba
-  externals: [nodeExternals()], 
   module: {
     rules: [
       {
@@ -51,4 +23,3 @@ export default {
   },
   plugins: [new MiniCssExtractPlugin()],
 };
-
