50cbf95bef34a5db7693aadcd6c69dbee535b4b1
root <root@DESKTOP-A93JRRE.localdomain>
2026-07-17 14:14:03 -0500

    cambios en proyecto 141

--- server/models/BaseModel.cjs
+++ server/models/BaseModel.cjs
@@ -1,56 +1,9 @@
-const objectionUnique = require('objection-unique');
-const BaseModel = require('./BaseModel.cjs');
-const encrypt = require('../lib/secure.cjs');
+// @ts-check
 
-const unique = objectionUnique({ fields: ['email'] });
+const { Model } = require('objection');
 
-module.exports = class User extends unique(BaseModel) {
-  static get tableName() {
-    return 'users';
+module.exports = class BaseModel extends Model {
+  static get modelPaths() {
+    return [__dirname];
   }
-
-  static get jsonSchema() {
-    return {
-      type: 'object',
-      required: ['firstName', 'lastName', 'email', 'password'],
-      properties: {
-        id: { type: 'integer' },
-        firstName: { type: 'string', minLength: 1 },
-        lastName: { type: 'string', minLength: 1 },
-        email: { type: 'string', minLength: 1 },
-        password: { type: 'string', minLength: 3 },
-      },
-    };
-  }
-
-  set password(value) {
-    this.passwordDigest = encrypt(value);
-  }
-
-  get name() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  verifyPassword(password) {
-    return encrypt(password) === this.passwordDigest;
-  }
-
-  static relationMappings = {
-    creatorTasks: {
-      relation: BaseModel.HasManyRelation,
-      modelClass: 'Task.cjs',
-      join: {
-        from: 'users.id',
-        to: 'tasks.creator_id',
-      },
-    },
-    executorTasks: {
-      relation: BaseModel.HasManyRelation,
-      modelClass: 'Task.cjs',
-      join: {
-        from: 'users.id',
-        to: 'tasks.executor_id',
-      },
-    },
-  };
 }
